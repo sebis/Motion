17bd0dd74879bd14aa8134fc06b5558f14c39e88
Sebastian Eriksson <sebastian.eriksson@iki.fi>
2011-10-17 08:34:08 +0300

    Fixed some pro file issues

--- common/generate_pro.sh
+++ common/generate_pro.sh
@@ -1,1 +1,1 @@
-qmake -project -t lib -recursive -after "CONFIG += staticlib" "LIBS += -Llib -lGL -lGLEW -lglut" "OBJECTS_DIR = obj" "DESTDIR = lib"
+qmake -project -t lib -r -after "CONFIG += staticlib" "LIBS += -Llib -lGL -lGLU -lGLEW -lglut" "OBJECTS_DIR = obj" "DESTDIR = lib"

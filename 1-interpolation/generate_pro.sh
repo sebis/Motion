14c9e2d85cb1f2223f3a0e77391421dc63fad7ac
Sebastian Eriksson <sebastian.eriksson@iki.fi>
2011-10-17 04:41:52 +0300

    Revert "Added makefiles to repository."
    This reverts commit 93be3a21c18f5ea6dd5c372ba7fb4e7929be5b9e.

--- 1-interpolation/generate_pro.sh
+++ 1-interpolation/generate_pro.sh
@@ -1,1 +1,1 @@
-qmake -project -t app -recursive -after "LIBS += -lGL -lGLEW -lSDL -lglut -L../common/lib -lcommon" "OBJECTS_DIR = obj" "INCLUDEPATH += ../common/include"
+qmake -project -t app -recursive -after "LIBS += -lGL -lGLEW -lSDL -L../common/lib -lcommon" "OBJECTS_DIR = obj" "INCLUDEPATH += ../common/include"

63ac015007740b73c83490591e81546fa0d64f3f
Sebastian Eriksson <sebastian.eriksson@iki.fi>
2012-02-14 22:45:25 +0200

    Added license headers.

--- engine/include/common/Transform.h
+++ engine/include/common/Transform.h
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2011-2012 Sebastian Eriksson.
+ * This file is part of Motion.
+ *
+ * Motion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Motion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Motion. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef COMMON_TRANSFORM
 #define COMMON_TRANSFORM
 
